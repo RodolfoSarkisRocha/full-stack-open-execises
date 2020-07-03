8ca44374f9b74c1dbed52ae70eba40398772f8be
Rodolfo Sarkis Rocha <rodolfosarkis.r@gmail.com>
2020-07-03 01:40:53 -0300

    Exercise 1.9

--- part1/unicafe/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/unicafe/src/components/Statistics.jsx
@@ -6,18 +6,31 @@ const Statistics = ({
     neutral,
     bad,
     average,
-    positive 
+    positive
   }
 }) => {
+  const getStatistics = () => {
+    
+    if (
+      good === 0 &&
+      neutral === 0 &&
+      bad === 0
+    ) return <div>No feedback given</div>
 
+    return (
+      <>
+        <div>{`Good ${good}`}</div>
+        <div>{`Neutral ${neutral}`}</div>
+        <div>{`Bad ${bad}`}</div>
+        <div>{`Average ${average}`}</div>
+        <div>{`Positive ${positive}%`}</div>
+      </>
+    )
+  }
   return (
     <>
       <h1>Statistics</h1>
-      <div>{`Good ${good}`}</div>
-      <div>{`Neutral ${neutral}`}</div>
-      <div>{`Bad ${bad}`}</div>
-      <div>{`Average ${average}`}</div>
-      <div>{`Positive ${positive}%`}</div>
+      {getStatistics()}
     </>
   )
 }
