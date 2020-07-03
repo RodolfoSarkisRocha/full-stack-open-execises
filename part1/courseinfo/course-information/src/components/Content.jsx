c5786e17511b8ecf4ce04a17259bd64406b4edf3
Rodolfo Sarkis Rocha <rodolfosarkis.r@gmail.com>
2020-07-03 00:29:49 -0300

    Moving Part component to outside

--- part1/courseinfo/course-information/src/components/Content.jsx
+++ part1/courseinfo/course-information/src/components/Content.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
+const Part = ({ part, exercises }) => (
+  <p>
+    {part} {exercises}
+  </p>
+)
+
 const Content = (props) => {
   const { parts } = props
-
-  const Part = ({ part, exercises }) => (
-    <p>
-      {part} {exercises}
-    </p>
-  )
 
   return (
     <>
