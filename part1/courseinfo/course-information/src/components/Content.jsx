df6338b5204375f042ec215a709263c4270e7167
Rodolfo Sarkis Rocha <rodolfosarkis.r@gmail.com>
2020-07-02 23:09:48 -0300

    Exercise 1.4

--- part1/courseinfo/course-information/src/components/Content.jsx
+++ part1/courseinfo/course-information/src/components/Content.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 
 const Content = (props) => {
-  const {
-    part1,
-    part2,
-    part3
-  } = props
+  const { parts } = props
 
   const Part = ({ part, exercises }) => (
     <p>
@@ -15,9 +11,9 @@ const Content = (props) => {
 
   return (
     <>
-      <Part part={part1.name} exercises={part1.exercises} />
-      <Part part={part2.name} exercises={part2.exercises} />
-      <Part part={part3.name} exercises={part3.exercises} />
+      {parts.map(currentPart => (
+        <Part part={currentPart.name} exercises={currentPart.exercises} />)
+      )}
     </>
   )
 }
--- part1/courseinfo/course-information/src/components/Total.jsx
+++ part1/courseinfo/course-information/src/components/Total.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
 const Total = (props) => {
-  const {
-    exercises1,
-    exercises2,
-    exercises3
-  } = props;
+  const { parts } = props;
+
+  const exercisesTotal = () => {
+    let sum = 0
+    parts.forEach(currentPart => {
+      sum += currentPart.exercises
+    })
+    return sum
+  }
 
   return (
     <p>
-      {`Number of exercises ${exercises1 + exercises2 + exercises3}`}
+      {`Number of exercises ${exercisesTotal()}`}
     </p>
   )
 }
